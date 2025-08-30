89a41deb1b45d83b85a82a3572ce7222bce5cbab
cuthbert9 <Cathbertjohnson9@gmail.com>
2025-08-30 14:18:54 +0300

    ds

--- src/components/frameworkCard.tsx
+++ src/components/frameworkCard.tsx
@@ -1,11 +1,15 @@
+import type { LucideIcon } from "lucide-react";
 
+interface FrameworkCardProps {
+    name: string;
+    Icon: LucideIcon; // or JSX.Element if you pass <Icon /> already
+}
 
-
-export default function FrameworkCard({ name, Icon }) {
+export default function FrameworkCard({ name, Icon }: FrameworkCardProps) {
     return (
         <div className="flex flex-col items-center justify-center w-40 h-40 p-4 rounded-2xl shadow-lg bg-white dark:bg-neutral-900">
             <Icon className="w-12 h-12 mb-3 text-blue-500" />
-            <p className="text-base text-black font-medium font-['italic'] text-center ">{name}</p>
+            <p className="text-center">{name}</p>
         </div>
-    )
+    );
 }
