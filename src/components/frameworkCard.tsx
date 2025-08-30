cfb15e9c84e6e85cf0b5f183545d151938bba29c
cuthbert9 <Cathbertjohnson9@gmail.com>
2025-08-30 14:11:25 +0300

    wrk

--- src/components/frameworkCard.tsx
+++ src/components/frameworkCard.tsx
@@ -1,10 +1,7 @@
 
-interface FrameworkCardProps {
-    name: string;
-    Icon: any;
-}
 
-export default function FrameworkCard({ name, Icon }: FrameworkCardProps) {
+
+export default function FrameworkCard({ name, Icon }) {
     return (
         <div className="flex flex-col items-center justify-center w-40 h-40 p-4 rounded-2xl shadow-lg bg-white dark:bg-neutral-900">
             <Icon className="w-12 h-12 mb-3 text-blue-500" />
